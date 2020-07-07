6386409c6902f5daf6446888376962e29d33fc4b
yonggyu.song <ygddabong@gmail.com>
2020-07-07 22:03:12 +0900

    second commit

--- django_projects/blog/blog/settings.py
+++ django_projects/blog/blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
     'entries',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
--- django_projects/blog/entries/views.py
+++ django_projects/blog/entries/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Entry
 
-class HomeView(ListView):
+class HomeView(LoginRequiredMixin, ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = "blog_entries"
     ordering = ['-entry_date']
     paginate_by = 3
 
-class EntryView(DetailView):
+class EntryView(LoginRequiredMixin, DetailView):
     model = Entry
     template_name = 'entries/entry_detail.html'
 
-class CreateEntryView(CreateView):
+class CreateEntryView(LoginRequiredMixin, CreateView):
     model = Entry
     template_name = 'entries/create_entry.html'
     fields = ['entry_title', 'entry_text']
